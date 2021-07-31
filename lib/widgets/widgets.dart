a3fe12cc3e8d182956710a95d878b4f821c8caa9
Hrishikesh Nikam <hrishinikam2000@gmail.com>
2021-07-31 23:51:54 +0530

    Add TextStyles

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_icons/flutter_icons.dart';
+// import 'package:flutter_icons/flutter_icons.dart';
 
 class SearchBar extends StatelessWidget {
   String hinttext;
